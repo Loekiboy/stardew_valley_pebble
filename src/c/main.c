f68ae308c6bf350fbe8809037b3d0fc2304238f9
Loekiboy <loekoerlemans@gmail.com>
2026-06-07 13:47:30 +0200

    Added background and rewrote in C

---
+++ src/c/main.c
@@ -0,0 +1,105 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+
+static GBitmap *s_background_bitmap;
+static BitmapLayer *s_background_layer;
+
+// Custom fonts declaratie
+static GFont s_time_font;
+static GFont s_date_font;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_time_buffer[8];
+  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_time_layer, s_time_buffer);
+
+  static char s_date_buffer[16];
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
+  text_layer_set_text(s_date_layer, s_date_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Load custom fonts
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_56));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_24));
+
+  // Center the time + date block vertically
+  int date_height = 30;
+  int block_height = 56 + date_height;
+  int time_y = (bounds.size.h / 2) - (block_height / 2) - 10;
+  int date_y = time_y + 56;
+
+  // Create the time TextLayer
+  s_time_layer = text_layer_create(GRect(0, time_y, bounds.size.w, 60));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Create the date TextLayer
+  s_date_layer = text_layer_create(GRect(0, date_y, bounds.size.w, date_height));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_font(s_date_layer, s_date_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Add tekst layers to the Window
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayers
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+
+  bitmap_layer_destroy(s_background_layer);
+  gbitmap_destroy(s_background_bitmap);
+
+  // Unload custom fonts
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  window_stack_push(s_main_window, true);
+  update_time();
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
