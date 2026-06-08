661e1779926675ecc1fef8ed06f9ef6958ad8905
Loekiboy <loekoerlemans@gmail.com>
2026-06-08 17:15:18 +0200

    Added the season badge

--- src/c/main.c
+++ src/c/main.c
@@ -10,9 +10,13 @@ static BitmapLayer *s_bg_layer;
 static GBitmap *s_fg_bitmap;
 static BitmapLayer *s_fg_layer;
 
-// Nieuwe variabelen voor de roterende wijzer (Sun/Moon dial)
+// Variabelen voor de roterende wijzer (Sun/Moon dial)
 static GBitmap *s_dial_bitmap;
 static RotBitmapLayer *s_dial_layer;
+
+static GBitmap *s_season_bitmap;
+static BitmapLayer *s_season_layer;
+static int s_current_season = -1;
 
 // Custom fonts
 static GFont s_time_font;
@@ -22,6 +26,7 @@ static void update_time() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
+  // --- TIJD EN DATUM ---
   static char s_time_buffer[8];
   strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
   text_layer_set_text(s_time_layer, s_time_buffer);
@@ -30,26 +35,66 @@ static void update_time() {
   strftime(s_date_buffer, sizeof(s_date_buffer), "%a. %d", tick_time);
   text_layer_set_text(s_date_layer, s_date_buffer);
 
-  // --- ROTATIE LOGICA ---
+// --- SEIZOENEN LOGICA ---
+  int month = tick_time->tm_mon;
+  int day = tick_time->tm_mday;
+  int detected_season = 0;
+
+  if ((month == 11 && day >= 21) || month == 0 || month == 1 || (month == 2 && day < 21)) {
+    detected_season = 0; // Winter: 21 dec t/m 20 mrt
+  } else if ((month == 2 && day >= 21) || month == 3 || month == 4 || (month == 5 && day < 21)) {
+    detected_season = 1; // Lente: 21 mrt t/m 20 jun
+  } else if ((month == 5 && day >= 21) || month == 6 || month == 7 || (month == 8 && day < 23)) {
+    detected_season = 2; // Zomer: 21 jun t/m 22 sep
+  } else {
+    detected_season = 3; // Herfst: 23 sep t/m 20 dec
+  }
+
+  if (detected_season != s_current_season) {
+    s_current_season = detected_season;
+
+    if (s_season_bitmap != NULL) {
+      gbitmap_destroy(s_season_bitmap);
+    }
+
+    switch (s_current_season) {
+      case 0:
+        s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_WINTER);
+        break;
+      case 1:
+        s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_SPRING);
+        break;
+      case 2:
+        s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_SUMMER);
+        break;
+      case 3:
+        s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_FALL);
+        break;
+    }
+
+    if (s_season_layer != NULL) {
+      bitmap_layer_set_bitmap(s_season_layer, s_season_bitmap);
+      layer_mark_dirty(bitmap_layer_get_layer(s_season_layer));
+    }
+  }
+
+  // --- ROTATIE LOGICA (DIAL) ---
   int hours = tick_time->tm_hour;
   int minutes = tick_time->tm_min;
-  int32_t angle = 0; // Standaard: 00:00 tot 06:00 (recht omhoog)
+  int32_t angle = 0; 
 
   if (hours >= 6) {
-    // Bereken minuten sinds 06:00
     int minutes_since_six = ((hours - 6) * 60) + minutes;
-    int total_minutes_interval = 1080; // 18 uur * 60 minuten
+    int total_minutes_interval = 1080;
     
-    int32_t start_angle = TRIG_MAX_ANGLE / 2; // 180 graden (recht omlaag)
-    int32_t total_rotation = TRIG_MAX_ANGLE / 2; // Moet nog 180 graden draaien tot 24:00
+    int32_t start_angle = TRIG_MAX_ANGLE / 2; // 180 graden
+    int32_t total_rotation = TRIG_MAX_ANGLE / 2; // Nog 180 graden te gaan
     
-    // Bereken vloeiende overgang
     angle = start_angle + ((minutes_since_six * total_rotation) / total_minutes_interval);
   } else {
     angle = 0; 
   }
 
-  // Pas de hoek toe op de wijzer
   rot_bitmap_layer_set_angle(s_dial_layer, angle);
 }
 
@@ -61,53 +106,53 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // 1. EERST: Achtergrond aanmaken en toevoegen
+  // 1. Achtergrond aanmaken
   s_bg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
   s_bg_layer = bitmap_layer_create(bounds);
   bitmap_layer_set_bitmap(s_bg_layer, s_bg_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bg_layer));
 
-  // 2. DAARNA: Voorgrond aanmaken en toevoegen
-  s_fg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FOREGROUND);
+  // Voorgrond afmetingen bepalen (nodig voor positionering seizoen)
   int fg_w = 180;
   int fg_h = 148;
   int fg_x = (bounds.size.w / 2) - (fg_w / 2); 
   int fg_y = 40;
 
+  int season_w = 30;
+  int season_h = 20;
+  int season_x = 142;
+  int season_y = 80;
+
+  s_season_layer = bitmap_layer_create(GRect(season_x, season_y, season_w, season_h));
+  bitmap_layer_set_compositing_mode(s_season_layer, GCompOpSet);
+  // We voegen hem NU al toe, zodat de voorgrond er dadelijk overheen valt!
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_season_layer));
+
+  // 3. Voorgrond aanmaken (valt over de seizoensafbeelding heen)
+  s_fg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FOREGROUND);
   s_fg_layer = bitmap_layer_create(GRect(fg_x, fg_y, fg_w, fg_h)); 
   bitmap_layer_set_bitmap(s_fg_layer, s_fg_bitmap);
   bitmap_layer_set_compositing_mode(s_fg_layer, GCompOpSet); 
   layer_add_child(window_layer, bitmap_layer_get_layer(s_fg_layer));
 
-// 3. De Roterende Wijzer toevoegen (zodat hij BOVENOP de voorgrond ligt!)
+  // 4. Roterende Dial aanmaken
   s_dial_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DIAL);
   s_dial_layer = rot_bitmap_layer_create(s_dial_bitmap);
-  
-  // DE FIX: de juiste functie zonder _layer_ erin
   rot_bitmap_set_compositing_mode(s_dial_layer, GCompOpSet);
   
-  // Bereken het rotatiepunt (onderkant van de afbeelding)
   GRect bitmap_bounds = gbitmap_get_bounds(s_dial_bitmap);
-  int width = bitmap_bounds.size.w;
-  int height = bitmap_bounds.size.h;
-  
-  GPoint pivot_point = GPoint(width / 2, height - 3);
+  GPoint pivot_point = GPoint(bitmap_bounds.size.w / 2, bitmap_bounds.size.h - 3);
   rot_bitmap_set_src_ic(s_dial_layer, pivot_point);
   
-  // Positionering op het scherm
   GRect dial_frame = layer_get_frame((Layer *)s_dial_layer);
-  dial_frame.origin.x = 28;  // Jouw test X coördinaat
-  dial_frame.origin.y = 55;  // Jouw test Y coördinaat
-  
-  // Sla de nieuwe positie op en voeg de laag toe aan het venster
+  dial_frame.origin.x = 28;
+  dial_frame.origin.y = 55;
   layer_set_frame((Layer *)s_dial_layer, dial_frame);
   layer_add_child(window_layer, (Layer *)s_dial_layer);
 
-  // 4. ALLES HIERONDER: Fonts en TextLayers (blijft hetzelfde)
+  // 5. Tekstlagen en lettertypen laden
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_28));
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_28));
-
-  int date_height = 30;
 
   s_time_layer = text_layer_create(GRect(28, 100, bounds.size.w, 60));
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -115,7 +160,7 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-  s_date_layer = text_layer_create(GRect(28, 45, bounds.size.w, date_height));
+  s_date_layer = text_layer_create(GRect(28, 45, bounds.size.w, 30));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorBlack);
   text_layer_set_font(s_date_layer, s_date_font);
@@ -132,7 +177,11 @@ static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_fg_layer);
   gbitmap_destroy(s_fg_bitmap);
 
-  // Ruim de wijzer netjes op
+  bitmap_layer_destroy(s_season_layer);
+  if (s_season_bitmap != NULL) {
+    gbitmap_destroy(s_season_bitmap);
+  }
+
   rot_bitmap_layer_destroy(s_dial_layer);
   gbitmap_destroy(s_dial_bitmap);
 
@@ -153,7 +202,7 @@ static void init() {
   });
 
   window_stack_push(s_main_window, true);
-  update_time();
+  update_time(); // Dit laadt ook direct het juiste seizoen in bij de start!
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
