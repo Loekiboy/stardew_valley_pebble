09b6295e6fa17355decb89683b949b8222241045
Loekiboy <loekoerlemans@gmail.com>
2026-06-09 18:27:39 +0200

    Added step counter

--- src/c/main.c
+++ src/c/main.c
@@ -23,7 +23,7 @@ static int s_current_season = -1;
 // Weer lagen
 static GBitmap *s_weather_bitmap;
 static BitmapLayer *s_weather_layer;
-static int s_current_weather = -1; 
+static int s_current_weather = -1;
 
 // Festival lagen
 static GBitmap *s_festival_bitmap;
@@ -32,6 +32,21 @@ static bool s_is_festival_day = false;
 
 static GFont s_time_font;
 static GFont s_date_font;
+static GFont s_steps_font;
+
+// --- STAPPEN ---
+static const GRect DIGIT_RECTS[6] = {
+  {{84,  150}, {20, 30}},  // slot 0 (meest links)
+  {{98,  150}, {20, 30}},  // slot 1
+  {{113,  150}, {20, 30}},  // slot 2
+  {{128,  150}, {20, 30}},  // slot 3
+  {{143,  150}, {20, 30}},  // slot 4
+  {{157, 150}, {20, 30}}   // slot 5 (meest rechts)
+};
+
+static TextLayer *s_digit_layers[6];
+static char s_digit_buffers[6][2];
+static char s_steps_buffer[16];
 
 // Functie om de exacte datum van het echte Pasen te berekenen (Meeus/Jones/Butcher algoritme)
 static bool is_easter(int year, int month, int day) {
@@ -47,8 +62,8 @@ static bool is_easter(int year, int month, int day) {
   int k = c % 4;
   int L = (32 + 2 * e + 2 * i - h - k) % 7;
   int m = (a + 11 * h + 22 * L) / 451;
-  
-  int easter_month = (h + L - 7 * m + 114) / 31; // 3 = Maart, 4 = April
+
+  int easter_month = (h + L - 7 * m + 114) / 31;
   int easter_day = ((h + L - 7 * m + 114) % 31) + 1;
 
   return (month == (easter_month - 1) && day == easter_day);
@@ -56,10 +71,56 @@ static bool is_easter(int year, int month, int day) {
 
 // Functie om te bepalen of het vandaag een festivaldag is
 static bool check_festival_day(int year, int month, int day) {
-  if (is_easter(year, month, day)) return true;  
+  if (is_easter(year, month, day)) return true;
   if (month == 11 && day == 25) return true; // Kerstmis (25 December)
   if (month == 0 && day == 1) return true;   // Nieuwjaar (1 Januari)
   return false;
+}
+
+static void update_digit_layers(const char *text) {
+  int len = strlen(text);
+
+  for (int i = 0; i < 6; i++) {
+    int text_index = len - 1 - i;
+
+    if (text_index >= 0) {
+      // Vul het slot met het bijbehorende karakter uit de tekst
+      s_digit_buffers[5 - i][0] = text[text_index];
+      s_digit_buffers[5 - i][1] = '\0';
+    } else {
+      // Als de tekst korter is dan 6 karakters, maken we de linkse slots leeg
+      s_digit_buffers[5 - i][0] = '\0';
+    }
+    
+    // Update de specifieke TextLayer van dit slot
+    text_layer_set_text(s_digit_layers[5 - i], s_digit_buffers[5 - i]);
+  }
+}
+
+static void update_steps() {
+  HealthMetric metric = HealthMetricStepCount;
+  time_t start = time_start_of_today();
+  time_t end = time(NULL);
+
+  // Controleer of de stappenteller beschikbaar is op het horloge
+  HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, start, end);
+
+  if (mask & HealthServiceAccessibilityMaskAvailable) {
+    // Haal het échte aantal stappen van vandaag op
+    HealthValue steps = health_service_sum_today(metric);
+    
+    snprintf(s_steps_buffer, sizeof(s_steps_buffer), "%d", (int)steps);
+  } else {
+    snprintf(s_steps_buffer, sizeof(s_steps_buffer), "---p");
+  }
+
+  update_digit_layers(s_steps_buffer);
+}
+
+static void health_handler(HealthEventType event, void *context) {
+  if (event == HealthEventMovementUpdate || event == HealthEventSignificantUpdate) {
+    update_steps();
+  }
 }
 
 static void update_time() {
@@ -67,8 +128,8 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   int year = tick_time->tm_year + 1900;
-  int month = tick_time->tm_mon; 
-  int day = tick_time->tm_mday;   
+  int month = tick_time->tm_mon;
+  int day = tick_time->tm_mday;
 
   // --- TIJD EN DATUM ---
   static char s_time_buffer[8];
@@ -99,7 +160,7 @@ static void update_time() {
       case 0: s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_WINTER); break;
       case 1: s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_SPRING); break;
       case 2: s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_SUMMER); break;
-      case 3: s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_FALL); break;
+      case 3: s_season_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SEASON_FALL);   break;
     }
     if (s_season_layer != NULL) {
       bitmap_layer_set_bitmap(s_season_layer, s_season_bitmap);
@@ -111,7 +172,7 @@ static void update_time() {
   bool today_festival = check_festival_day(year, month, day);
   if (today_festival != s_is_festival_day) {
     s_is_festival_day = today_festival;
-    
+
     if (s_festival_bitmap != NULL) {
       gbitmap_destroy(s_festival_bitmap);
       s_festival_bitmap = NULL;
@@ -120,7 +181,7 @@ static void update_time() {
     if (s_is_festival_day) {
       s_festival_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BADGE_FESTIVAL);
       bitmap_layer_set_bitmap(s_festival_layer, s_festival_bitmap);
-      
+
       // Wissel om: Verberg seizoen, laat festival zien
       layer_set_hidden(bitmap_layer_get_layer(s_season_layer), true);
       layer_set_hidden(bitmap_layer_get_layer(s_festival_layer), false);
@@ -134,15 +195,15 @@ static void update_time() {
   // --- ROTATIE LOGICA ---
   int hours = tick_time->tm_hour;
   int minutes = tick_time->tm_min;
-  int32_t angle = 0; 
+  int32_t angle = 0;
   if (hours >= 6) {
     int minutes_since_six = ((hours - 6) * 60) + minutes;
     int total_minutes_interval = 1080;
-    int32_t start_angle = TRIG_MAX_ANGLE / 2; 
-    int32_t total_rotation = TRIG_MAX_ANGLE / 2; 
+    int32_t start_angle = TRIG_MAX_ANGLE / 2;
+    int32_t total_rotation = TRIG_MAX_ANGLE / 2;
     angle = start_angle + ((minutes_since_six * total_rotation) / total_minutes_interval);
   } else {
-    angle = 0; 
+    angle = 0;
   }
   rot_bitmap_layer_set_angle(s_dial_layer, angle);
 }
@@ -152,17 +213,17 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   Tuple *weather_tuple = dict_find(iterator, WEATHER_KEY);
   if (weather_tuple) {
     int received_weather = weather_tuple->value->int32;
-    
+
     if (received_weather != s_current_weather) {
       s_current_weather = received_weather;
       if (s_weather_bitmap != NULL) gbitmap_destroy(s_weather_bitmap);
 
       switch (s_current_weather) {
-        case 0: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_SUN); break;
-        case 1: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_RAIN); break;
-        case 2: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_SNOW); break;
+        case 0: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_SUN);     break;
+        case 1: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_RAIN);    break;
+        case 2: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_SNOW);    break;
         case 3: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_THUNDER); break;
-        case 4: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_WIND); break;
+        case 4: s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WEATHER_WIND);    break;
       }
 
       if (s_weather_layer != NULL && s_weather_bitmap != NULL) {
@@ -175,7 +236,7 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-  
+
   if (tick_time->tm_min % 30 == 0) {
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
@@ -198,10 +259,10 @@ static void main_window_load(Window *window) {
 
   int item_w = 30;
   int item_h = 20;
-  
+
   int season_x = 142;
   int season_y = 80;
-  int badge_spacing_x = 60; 
+  int badge_spacing_x = 60;
 
   // 2. Seizoenen laag
   s_season_layer = bitmap_layer_create(GRect(season_x, season_y, item_w, item_h));
@@ -228,12 +289,12 @@ static void main_window_load(Window *window) {
   s_fg_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_FOREGROUND);
   int fg_w = 180;
   int fg_h = 148;
-  int fg_x = (bounds.size.w / 2) - (fg_w / 2); 
+  int fg_x = (bounds.size.w / 2) - (fg_w / 2);
   int fg_y = 40;
-  
-  s_fg_layer = bitmap_layer_create(GRect(fg_x, fg_y, fg_w, fg_h)); 
+
+  s_fg_layer = bitmap_layer_create(GRect(fg_x, fg_y, fg_w, fg_h));
   bitmap_layer_set_bitmap(s_fg_layer, s_fg_bitmap);
-  bitmap_layer_set_compositing_mode(s_fg_layer, GCompOpSet); 
+  bitmap_layer_set_compositing_mode(s_fg_layer, GCompOpSet);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_fg_layer));
 
   // 6. Roterende Dial
@@ -249,7 +310,7 @@ static void main_window_load(Window *window) {
   layer_set_frame((Layer *)s_dial_layer, dial_frame);
   layer_add_child(window_layer, (Layer *)s_dial_layer);
 
-  // Textlagen
+  // 7. Tekstlagen: Datum en Tijd
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_28));
   s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SV_28));
 
@@ -267,14 +328,35 @@ static void main_window_load(Window *window) {
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+
+  // 8. Stappen
+  s_steps_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_STEPS_28));
+
+  for (int i = 0; i < 6; i++) {
+    s_digit_layers[i] = text_layer_create(DIGIT_RECTS[i]);
+    text_layer_set_background_color(s_digit_layers[i], GColorClear);
+    text_layer_set_text_color(s_digit_layers[i], GColorFromHEX(0x7e050a));
+    text_layer_set_font(s_digit_layers[i], s_steps_font);
+    text_layer_set_text_alignment(s_digit_layers[i], GTextAlignmentLeft);
+    text_layer_set_text(s_digit_layers[i], "");
+    layer_add_child(window_layer, text_layer_get_layer(s_digit_layers[i]));
+  }
+
+  // Direct de huidige stappentelling laden
+  update_steps();
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+
+  for (int i = 0; i < 6; i++) {
+    text_layer_destroy(s_digit_layers[i]);
+  }
+
   bitmap_layer_destroy(s_fg_layer);
   gbitmap_destroy(s_fg_bitmap);
-  
+
   bitmap_layer_destroy(s_season_layer);
   if (s_season_bitmap != NULL) gbitmap_destroy(s_season_bitmap);
 
@@ -288,8 +370,10 @@ static void main_window_unload(Window *window) {
   gbitmap_destroy(s_dial_bitmap);
   bitmap_layer_destroy(s_bg_layer);
   gbitmap_destroy(s_bg_bitmap);
+
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_date_font);
+  fonts_unload_custom_font(s_steps_font);
 }
 
 static void init() {
@@ -300,16 +384,19 @@ static void init() {
     .unload = main_window_unload
   });
   window_stack_push(s_main_window, true);
-  
+
   update_time();
-  
+
   app_message_register_inbox_received(inbox_received_callback);
   app_message_open(64, 64);
-  
+
+  health_service_events_subscribe(health_handler, NULL);
+
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
+  health_service_events_unsubscribe();
   window_destroy(s_main_window);
 }
 
